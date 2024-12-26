9bfa9bd00f85b4249b8978aab95a9648be19b2d6
Eric Blanvillain <b00684319@essec.edu>
2024-12-26 09:28:00 +0100

    Add GitHub Actions workflow and tests setup

---
+++ tests/test_query_processor.py
@@ -0,0 +1,14 @@
+import pytest
+from src.query_processor import QueryProcessor
+
+def test_detect_query_type():
+    # TODO: Implement test
+    pass
+
+def test_calculate_content_relevance():
+    # TODO: Implement test
+    pass
+
+def test_get_initial_results():
+    # TODO: Implement test
+    pass
---
+++ tests/test_ui_components.py
@@ -0,0 +1,10 @@
+import pytest
+from src.ui_components import UIComponents
+
+def test_create_controls():
+    # TODO: Implement test
+    pass
+
+def test_display_results():
+    # TODO: Implement test
+    pass
